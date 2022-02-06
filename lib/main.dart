84b1d325bef032640e70a89dbe84164e60bd1dcd
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-02-06 16:37:52 +0700

    refactor(main.dart): remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:talk_o_bloc/routers/app_router.dart';
-import 'package:talk_o_bloc/store/bloc/quote_bloc.dart';
-import 'package:talk_o_bloc/widgets/base/count.dart';
-import 'package:talk_o_bloc/widgets/base/list_quote.dart';
-import 'package:talk_o_bloc/widgets/base/floating_button/add_quote_button.dart';
-import 'package:talk_o_bloc/widgets/base/floating_button/increment_max_quote_button.dart';
-import 'package:talk_o_bloc/widgets/base/floating_button/delete_quote_button.dart';
-import 'package:talk_o_bloc/widgets/base/floating_button/reset_quote_button.dart';
-import 'package:talk_o_bloc/widgets/screens/home_screen.dart';
 
 void main() {
   runApp(App());
