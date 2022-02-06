69530445f98170e088f2ac5ddb737c8bcb4d4aa0
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-02-06 12:34:14 +0700

    feat(count.dart): add color if quotes length reach max

--- lib/widgets/count.dart
+++ lib/widgets/count.dart
@@ -8,34 +8,50 @@ class Count extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var isFull = context.select<QuoteBloc, bool>((value) => value.state.quotes.length >= value.state.max);
     return Column(
       children: [
         const Text("Total"),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BlocSelector<QuoteBloc, QuoteState, int>(
-              selector: (state) {
-                return state.quotes.length;
-              }, 
-              builder: (context, length) {
-                return Text(length.toString(), style: Theme.of(context).textTheme.headline3);
-              }
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 5),
-              child: Text("/"),
-            ),
-            BlocSelector<QuoteBloc, QuoteState, int>(
-              selector: (state) {
-                return state.max;
-              }, 
-              builder: (context, max) {
-                return Text(max.toString(), style: Theme.of(context).textTheme.headline5);
-              }
-            ),
-          ],
+        DefaultTextStyle(
+          style: TextStyle(
+            color: isFull ? Colors.red : Colors.black
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              BlocSelector<QuoteBloc, QuoteState, int>(
+                selector: (state) {
+                  return state.quotes.length;
+                }, 
+                builder: (context, length) {
+                  return Text(
+                    length.toString(),
+                    style: TextStyle(
+                      fontSize: Theme.of(context).textTheme.headline3?.fontSize
+                    )
+                  );
+                }
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 5),
+                child: Text("/"),
+              ),
+              BlocSelector<QuoteBloc, QuoteState, int>(
+                selector: (state) {
+                  return state.max;
+                }, 
+                builder: (context, max) {
+                  return Text(
+                    max.toString(),
+                    style: TextStyle(
+                      fontSize: Theme.of(context).textTheme.headline5?.fontSize
+                    ),
+                  );
+                }
+              ),
+            ],
+          )
         )
       ],
     );
