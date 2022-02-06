6a29e24667220e6f338276be8b65263862c867d9
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-02-06 12:06:03 +0700

    feat(count.dart): show max

--- lib/widgets/count.dart
+++ lib/widgets/count.dart
@@ -11,13 +11,31 @@ class Count extends StatelessWidget {
     return Column(
       children: [
         const Text("Total"),
-        BlocSelector<QuoteBloc, QuoteState, int>(
-          selector: (state) {
-            return state.quotes.length;
-          }, 
-          builder: (context, state) {
-            return Text(state.toString(), style: Theme.of(context).textTheme.headline3);
-          }
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BlocSelector<QuoteBloc, QuoteState, int>(
+              selector: (state) {
+                return state.quotes.length;
+              }, 
+              builder: (context, length) {
+                return Text(length.toString(), style: Theme.of(context).textTheme.headline3);
+              }
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5),
+              child: Text("/"),
+            ),
+            BlocSelector<QuoteBloc, QuoteState, int>(
+              selector: (state) {
+                return state.max;
+              }, 
+              builder: (context, max) {
+                return Text(max.toString(), style: Theme.of(context).textTheme.headline5);
+              }
+            ),
+          ],
         )
       ],
     );
