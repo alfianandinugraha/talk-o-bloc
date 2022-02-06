e53e8f2eab339fdbb5e8b7083eed7d31383ed46a
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-02-06 12:07:21 +0700

    feat(add_quote_button.dart): change background color if quotes length reach the limit

--- lib/widgets/add_quote_button.dart
+++ lib/widgets/add_quote_button.dart
@@ -3,23 +3,30 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:talk_o_bloc/models/quote.dart';
 import 'package:talk_o_bloc/store/bloc/quote_bloc.dart';
 import 'package:talk_o_bloc/store/events/quote_event.dart';
+import 'package:talk_o_bloc/store/state/quote_state.dart';
 
 class AddQuoteButton extends StatelessWidget {
   const AddQuoteButton({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(
-      onPressed: () {
-        BlocProvider
-          .of<QuoteBloc>(context)
-          .add(
-            AddQuoteEvent(
-              quote: Quote(text: "Hello world !") 
-            )
-          );
-      },
-      child: const Icon(Icons.add),
+    return BlocSelector<QuoteBloc, QuoteState, bool>(
+      selector: (state) => state.quotes.length >= state.max,
+      builder: (context, isFull) {
+        return FloatingActionButton(
+          onPressed: () {
+            BlocProvider
+              .of<QuoteBloc>(context)
+              .add(
+                AddQuoteEvent(
+                  quote: Quote(text: "Hello world !") 
+                )
+              );
+          },
+          child: const Icon(Icons.add),
+          backgroundColor: isFull ? Colors.grey : Colors.red,
+        );
+      }
     );
   }
 }
